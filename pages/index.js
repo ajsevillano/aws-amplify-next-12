0aacb759431524f5eec4f60d0bbff6110443f8fc
Antonio Sevillano <antonio.sevillano@percayso.com>
2022-07-08 13:58:29 +0100

    Fixed useEffect dependencies

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ breeds = [] }) {
 
   useEffect(() => {
     setBreedList(Object.keys(breeds));
-  }, []);
+  }, [breeds]);
 
   return (
     <div className={styles.container}>
